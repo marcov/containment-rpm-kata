8c169870b0e302a336b507818509c223b9c550ae
Marco Vedovati <mvedovati@suse.com>
2018-11-30 18:03:07 +0100

    Add init link

--- build_kata_img.sh
+++ build_kata_img.sh
@@ -7,5 +7,9 @@ readonly img="$2"
 readonly script_dir="$(dirname $(readlink -f $0))"
 
 echo "Will create some new image from $img in $rootfsDir"
+
+# Create /sbin/init (why does not exist?)
+ln -s /usr/lib/systemd/systemd ${rootfsDir}/sbin/init
+
 ${script_dir}/osbuilder/image-builder/image_builder.sh -o "$img" "$rootfsDir"
 
