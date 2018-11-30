da1eb2c41060810ac90dfe84ba12d3233f21b3ed
Marco Vedovati <mvedovati@suse.com>
2018-11-30 17:48:54 +0100

    fix errs

--- build_kata_img.sh
+++ build_kata_img.sh
@@ -4,10 +4,8 @@ set -euo pipefail
 
 readonly rootfsDir="$1"
 readonly img="$2"
-
-script_dir="$(dirname $(readlink -f $0))"
+readonly script_dir="$(dirname $(readlink -f $0))"
 
 echo "Will create some new image from $img in $rootfsDir"
-
-${script_dir}/osbuilder/image-builder/image-builder.sh -o "$img" "$rootfsDir"
+${script_dir}/osbuilder/image-builder/image_builder.sh -o "$img" "$rootfsDir"
 
