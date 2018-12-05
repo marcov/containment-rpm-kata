7ad7b550e12e12a5d8fcb51381ef59fcd5e35625
Marco Vedovati <mvedovati@suse.com>
2018-12-05 17:09:07 +0100

    Add provide

--- build_kata_img.sh
+++ build_kata_img.sh
@@ -8,9 +8,9 @@ readonly script_dir="$(dirname $(readlink -f $0))"
 
 echo "Will create some new image from $img in $rootfsDir"
 
-# Create /sbin/init (why does not exist?)
+# Create /sbin/init if it does not exist
 [ -e "${rootfsDir}/sbin/init" ] || ln -s /usr/lib/systemd/systemd ${rootfsDir}/sbin/init
-# Create /lib/systemd/systemd (why does not exist?)
+# Create /lib/systemd/systemd if it does not exist
 [ -e "${rootfsDir}/lib/systemd/systemd" ] || (mkdir -p ${rootfsDir}/lib/systemd/; ln -s /usr/lib/systemd/systemd ${rootfsDir}/lib/systemd/systemd)
 
 ${script_dir}/osbuilder/image-builder/image_builder.sh -o "$img" "$rootfsDir"
