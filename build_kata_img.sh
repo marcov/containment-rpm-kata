6e9343bb512bf180b99b3536d8f1217f090fadbe
Marco Vedovati <mvedovati@suse.com>
2018-12-17 18:32:34 +0100

    Fill osbuilder yaml variables

--- build_kata_img.sh
+++ build_kata_img.sh
@@ -5,10 +5,22 @@ readonly rootfsDir="$1"
 readonly targetImg="$2"
 readonly script_dir="$(dirname $(readlink -f $0))"
 
-source ${script_dir}/kata-osbuilder/scripts/lib.sh
-
 echo "Creating kata image $targetImg from $rootfsDir"
-create_summary_file "$rootfsDir"
+
+# TODO: consider retrieve variables calling rootfs.sh
+gen_summary() {
+	local OSBUILDER_VERSION=$(rpm -q kata-osbuilder)
+	local AGENT_INIT="no"
+	local AGENT_BIN="kata-agent"
+	local AGENT_DEST="${rootfsDir}/usr/bin/${AGENT_BIN}"
+	local ARCH="$(arch)"
+	local GO_AGENT_PKG="github.com/kata-containers/agent"
+	local OS_VERSION="(built with kiwi)"
+	local OS_NAME="openSUSE"
+	source ${script_dir}/kata-osbuilder/scripts/lib.sh
+	create_summary_file "$rootfsDir"
+}
+gen_summary
 
 # Create /sbin/init if it does not exist
 [ -e "${rootfsDir}/sbin/init" ] || ln -s /usr/lib/systemd/systemd ${rootfsDir}/sbin/init
